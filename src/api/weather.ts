d73945d12d82ba037c7ae5f1f10a998077b59e9d
dimakey <dkrapivnyy@gmail.com>
2026-01-20 16:23:17 +0500

    fix(api): switch API base URL to https

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { GeoCoords } from "../types/types";
 
 const axiosWeatherApi = axios.create({
-  baseURL: "http://api.weatherapi.com/v1/forecast.json",
+  baseURL: "https://api.weatherapi.com/v1/forecast.json",
   params: {
     key: import.meta.env.VITE_WEATHERS_API,
     days: "7",
